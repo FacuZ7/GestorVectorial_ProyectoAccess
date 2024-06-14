a21f6be0ddaf094e9f5a3b8b8675a165672dc4af
Facundo Zapata <facundozapata41@gmail.com>
2024-06-13 22:53:34 -0300

    push parcial para continuar

--- src/app.js
+++ src/app.js
@@ -1,13 +1,41 @@
-import 'dotenv/config';
-import { manageEmbeddings } from './handlers/embeddingsHandler.js';
-import { pineconeClient } from './config/pineconeConfig.js';
-import { loadDocuments } from './utils/documentLoader.js';
-import { splitDocuments } from './utils/textSplitter.js';
-const listIndexes = await pineconeClient.listIndexes()
+import * as dotenv from "dotenv";
+// import { manageEmbeddings } from './handlers/embeddingsHandler.js';
+import { pineconeClient, indexName } from './config/pineconeConfig.js';
+import { getEmbedding } from "./services/pineconeService.js";
+//import { loadDocuments } from './utils/documentLoader.js';
+//import { splitDocuments } from './utils/textSplitter.js';
+dotenv.config();
 
-console.log(listIndexes)
-const docs = await loadDocuments("src/data")
-const splittedDocs = await splitDocuments(docs)
+/********************* PRUEBAS PINECONE **************************************/
+const listIndexes = await pineconeClient.listIndexes() //=> listar todos mis indices
+const index = pineconeClient.index(indexName) //=> busca el indice deseado.
+//console.log(listIndexes)
+//console.log(index) 
+
+console.log(await getEmbedding("99"))
+
+/************************* loadDocuments *********************************/
+// const docs = await loadDocuments("src/data") => encuentra la carpeta de los txts con información.
+// console.log(docs)
+/************************* splitDocuments *********************************/
+
+// const docs = await loadDocuments("src/data")
+// const splittedDocuments = await splitDocuments(docs)
+
+// console.log(splittedDocuments.toString()) //salen cosas raras pero funciona 
+//=> esto guardo en PINECONE
+
+/**************************************************************************/
+
+
+
+
+
+
+//console.log(listIndexes.Index(indexName))
+
+// 
+// const splittedDocs = await splitDocuments(docs)
 //continuar desde splitted docs, tengo que continuar lo que haga 
 //vectorStore del back y usar pinecone en lugar de Faiss
 
---
+++ src/utils/vectorStore.js
@@ -0,0 +1,1 @@
+import { PineconeStore } from "@langchain/pinecone"
