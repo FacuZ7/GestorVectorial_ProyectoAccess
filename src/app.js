1b0d7fb5bace8e3b409d1d9850a324c4b95b81fe
Facundo Zapata <facundozapata41@gmail.com>
2024-06-14 01:15:24 -0300

    avance parcial de creacion de indices si no existen + creacion e incrustacion de vectores

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import * as dotenv from "dotenv";
 // import { manageEmbeddings } from './handlers/embeddingsHandler.js';
 import { pineconeClient, indexName } from './config/pineconeConfig.js';
-import { getEmbedding } from "./services/pineconeService.js";
-//import { loadDocuments } from './utils/documentLoader.js';
-//import { splitDocuments } from './utils/textSplitter.js';
+import openaiEmbeddings from "./config/openaiConfig.js";
+import { PineconeStore } from "@langchain/pinecone";
+import { loadDocuments } from './utils/documentLoader.js';
+import { splitDocuments } from './utils/textSplitter.js';
 dotenv.config();
 
 /********************* PRUEBAS PINECONE **************************************/
-const listIndexes = await pineconeClient.listIndexes() //=> listar todos mis indices
-const index = pineconeClient.index(indexName) //=> busca el indice deseado.
-//console.log(listIndexes)
-//console.log(index) 
+// const listIndexes = await pineconeClient.listIndexes() //=> listar todos mis indices
+// const index = pineconeClient.index(indexName) //=> busca el indice deseado.
+// console.log("todos los indices: ", listIndexes)
+// console.log("el indice buscado: ", index) 
 
-console.log(await getEmbedding("99"))
 
 /************************* loadDocuments *********************************/
 // const docs = await loadDocuments("src/data") => encuentra la carpeta de los txts con información.
@@ -25,16 +25,72 @@ console.log(await getEmbedding("99"))
 // console.log(splittedDocuments.toString()) //salen cosas raras pero funciona 
 //=> esto guardo en PINECONE
 
-/**************************************************************************/
+/************************************* ESTO SIRVE PARA CREAR LOS EMBEDDINGS Y GUARDARLOS EN PINECONE *************************************/
+// const pineconeIndex = pineconeClient.index(indexName)
+// const docs = await loadDocuments(process.env.KNOWLEDGE_PATH)
+// const splittedDocuments = await splitDocuments(docs)
+
+// console.log(splittedDocuments.toString())   //salen cosas raras pero funciona 
+//                                             //=> esto guardo en PINECONE
+
+// const vectorStore = await PineconeStore.fromDocuments(splittedDocuments, openaiEmbeddings, {
+//     pineconeIndex,
+//     maxConcurrency: 5
+// })
+
+/*********************************************************************************************************************************/
+// SI HAY RESULT, SIGNIFICA QUE CREÓ EL INDICE Y LO DEVUELVE.
+// SI EL INDICE EXISTE, NO LO CREA Y DEVUELVE UNDEFINED PORQUE NO HIZO NADA.
+// const result = await pineconeClient.createIndex({
+//   name: indexName,
+//   dimension: process.env.PINECONE_INDEX_DIMENSION,
+//   metric: 'cosine',
+//   spec: { 
+//     serverless: { 
+//       cloud: process.env.PINECONE_CLOUD, 
+//       region: process.env.PINECONE_REGION 
+//     }
+//   },
+//   suppressConflicts: true
+// }); 
+
+
+// console.log(result)
+
+// todo junto y en orden
+const docs = await loadDocuments(process.env.KNOWLEDGE_PATH)
+const splittedDocuments = await splitDocuments(docs)
+
+await pineconeClient.createIndex({
+    name: indexName,
+    dimension: 1536,
+    metric: 'cosine',
+    spec: { 
+    serverless: { 
+        cloud: process.env.PINECONE_CLOUD, 
+        region: process.env.PINECONE_REGION 
+    }
+    },
+    suppressConflicts: true,
+    waitUntilReady: true
+})
+
+const pineconeIndex = pineconeClient.index(indexName)
+
+const vectorStore = await PineconeStore.fromDocuments(splittedDocuments, openaiEmbeddings, {
+    pineconeIndex,
+    maxConcurrency: 5
+})
 
 
 
 
 
 
-//console.log(listIndexes.Index(indexName))
 
-// 
+
+
+
 // const splittedDocs = await splitDocuments(docs)
 //continuar desde splitted docs, tengo que continuar lo que haga 
 //vectorStore del back y usar pinecone en lugar de Faiss
--- src/services/openaiService.js
+++
@@ -1,6 +0,0 @@
-import openaiEmbeddings from '../config/openaiConfig.js';
-
-export async function generateEmbeddings(text) {
-    const embeddings = await openaiEmbeddings.embed(text);
-    return embeddings;
-}
