4b4f7aed50c162968ec780b2422fe676eed7e43b
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2024-06-21 22:15:22 -0300

    Se agrega loader de pdf y docx

--- src/utils/documentLoader.js
+++ src/utils/documentLoader.js
@@ -1,11 +1,15 @@
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { TextLoader } from "langchain/document_loaders/fs/text";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { DocxLoader } from "@langchain/community/document_loaders/fs/docx";
 
 export const loadDocuments = async (directory)=>{
     const loader = new DirectoryLoader(
         directory,
         {
           ".txt": (path) => new TextLoader(path),
+          ".pdf": (path) => new PDFLoader(path, { splitPages: false }),
+          ".docx": (path) => new DocxLoader(path) // Para pares pregunta respuesta validados
         }
     );
 
