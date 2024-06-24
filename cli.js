deeed6430b37522bf940989f3ef8df2aea8369fc
Facundo Zapata <facundozapata41@gmail.com>
2024-06-24 20:29:07 -0300

    Se saca el comando renombrar archivos ya que no se llegó a implementar

--- cli.js
+++ cli.js
@@ -3,10 +3,7 @@ import formatFileNames from "./src/utils/formatFileNames.js";
 const command = process.argv[2];
 
 switch (command) {
-    case 'formatfilenames':    
-        formatFileNames();
-        break;
     default:
-        console.log("comando sin implementación")
+        console.log("Comandos sin implementación")
         break;
 }
